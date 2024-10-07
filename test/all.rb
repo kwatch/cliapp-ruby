56ad7b37a3ec51edb104d010d4d8bbbfe4610fa6
kwatch <kwatch@gmail.com>
2024-10-07 17:53:01 +0900

    fix 'test/all.rb' to support Oktest 1.5

--- test/all.rb
+++ test/all.rb
@@ -12,7 +12,11 @@ end
 if __FILE__ == $0
   require 'oktest'
   Oktest.module_eval do
-    remove_const :REPORTER_CLASS
-    const_set :REPORTER_CLASS, Oktest::PlainReporter
+    if defined?(REPORTER_CLASS)       # Oktest < 1.5
+      remove_const :REPORTER_CLASS
+      const_set :REPORTER_CLASS, Oktest::PlainReporter
+    else                              # Oktst >= 1.5
+      self.DEFAULT_REPORTING_STYLE = "plain"
+    end
   end
 end
