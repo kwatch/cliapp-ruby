60967611adb672ffb2debe6bb651c05050a70e8e
kwatch <kwatch@gmail.com>
2024-10-09 05:25:29 +0900

    update 'Rakefile.rb'

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,9 +1,13 @@
-# -*- coding: utf-8 -*-
+# coding: utf-8
+# frozen_string_literal: true
 
 
 PROJECT       = "cliapp"
 COPYRIGHT     = "copyright(c) 2024 kwatch@gmail.com"
 
-RUBY_VERSIONS = %w[3.3 3.2 3.1 3.0 2.7 2.6 2.5 2.4]
+RUBY_VERSIONS = %w[3.3 3.2 3.1 3.0 2.7 2.6 2.5 2.4]  # for 'test:all' task
 
-Dir.glob("./task/*-task.rb").each {|x| require x }
+task :default => :help   # or :test if you prefer
+
+require_relative "./rake/init"
+Dir.glob("./rake/*_task.rb").sort.each {|x| require x }
