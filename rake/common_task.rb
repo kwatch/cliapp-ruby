4b5c9314b07edae034a1ddd709780530c4e46db5
kwatch <kwatch@gmail.com>
2024-10-10 20:44:45 +0900

    remove 'test' task from 'rake/common_task.rb'

--- rake/common_task.rb
+++ rake/common_task.rb
@@ -2,8 +2,9 @@
 # frozen_string_literal: true
 
 
-task :default => :help    # or :test if you like
-
+##
+## help
+##
 
 desc "list task names"
 task :help do
@@ -11,24 +12,19 @@ task :help do
 end
 
 
-desc "run test scripts"
-task :test do
-  $LOAD_PATH << File.join(File.dirname(__FILE__), "lib")
-  sh "oktest test -sp"
+##
+## clean
+##
+
+desc "delete garbage files"
+task :clean do
+  rm_f Dir.glob(GARBAGE_FILES)
 end
 
-
-desc "run test scripts on Ruby 2.x and 3.x"
-task :'test:all' do
-  vs_home = ENV['VS_HOME']  or raise "$VS_HOME should be set."
-  defined?(RUBY_VERSIONS)  or raise "RUBY_VERSIONS should be defined."
-  $LOAD_PATH << File.join(File.dirname(__FILE__), "lib")
-  RUBY_VERSIONS.each do |ver|
-    path_pat = "#{vs_home}/ruby/#{ver}.*/bin/ruby"
-    ruby_path = Dir.glob(path_pat).sort.last()  or
-      raise "#{path_pat}: Not exist."
-    puts "\e[33m======== Ruby #{ver} ========\e[0m"
-    sh "#{ruby_path} -r oktest -e 'Oktest.main' -- test -sp" do end
-    puts ""
-  end
+desc "delete garbage and product files"
+task "clean:all" => :clean do
+  rm_f Dir.glob(PRODUCT_FILES)
 end
+
+GARBAGE_FILES = ["**/*~", "**/*.bak", "**/core", "#{PROJECT}-*.*.*/"]
+PRODUCT_FILES = ["#{PROJECT}-*.gem"]
